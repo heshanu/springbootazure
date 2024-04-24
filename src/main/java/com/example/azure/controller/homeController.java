8ec51cd88ddd310f6af810fa2d3739447d18ba5d
heshanu <heshanu97@gmail.com>
2024-04-25 01:55:39 +0530

    updated

--- src/main/java/com/example/azure/controller/homeController.java
+++ src/main/java/com/example/azure/controller/homeController.java
@@ -1,8 +1,8 @@
 package com.example.azure.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class homeController {
     @GetMapping("/")
