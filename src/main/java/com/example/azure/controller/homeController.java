3785e2bc87ddd16709a7e978ea61e91e38f0fd98
Heshan Umayanga <38934645+heshanu@users.noreply.github.com>
2024-04-25 01:26:51 +0530

    Update homeController.java

--- src/main/java/com/example/azure/controller/homeController.java
+++ src/main/java/com/example/azure/controller/homeController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/home")
 public class homeController {
+    @GetMapping("/")
     public String hello(){
         return "hello";
     }
